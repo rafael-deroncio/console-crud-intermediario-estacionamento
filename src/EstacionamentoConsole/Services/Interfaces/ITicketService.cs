f466b7c807997fb1aa0dadbd9829c9d5ff575d04
rderoncio <rafael.deroncio@animaeducacao.com.br>
2023-08-08 10:13:47 -0300

    add services

---
+++ src/EstacionamentoConsole/Services/Interfaces/ITicketService.cs
@@ -0,0 +1,11 @@
+﻿using Estacionamento.Models;
+
+namespace Estacionamento.Services.Interfaces
+{
+    public interface ITicketService
+    {
+        Task<List<TicketModel>> GetTickets();
+        Task<bool> AtualizarTicket(TicketModel ticket, bool msgStatus = true);
+        Task<bool> CadastrarTicket(TicketModel ticket, bool msgStatus = true);
+    }
+}
---
+++ src/EstacionamentoConsole/Services/Interfaces/IVeiculoService.cs
@@ -0,0 +1,11 @@
+﻿using Estacionamento.Models;
+
+namespace Estacionamento.Services.Interfaces
+{
+    public interface IVeiculoService
+    {
+        Task<List<VeiculoModel>> GetVeiculos();
+        Task<VeiculoModel> GetVeiculo(int id);
+        Task<bool> CadastrarVeiculo(VeiculoModel veiculo, bool msgStatus = true);
+    }
+}
